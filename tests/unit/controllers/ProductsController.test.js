a34a6ac67f3b8646e63a5827643aeab49ebf44e7
Joao <joaovictorcgjvcg@gmail.com>
2022-07-05 18:12:55 -0400

    entregando o teste da funcao addNewProduct do controller

--- tests/unit/controllers/ProductsController.test.js
+++ tests/unit/controllers/ProductsController.test.js
@@ -19,15 +19,49 @@ describe('ProductsController', () => {
       ProductsService.getAll.restore();
     })
 
-    it('é retornado o status 200', async () => {
+    it('deve retornar o "status" 200', async () => {
       await ProductsController.getAll(request, response)
 
       expect(response.status.calledWith(200)).to.be.equal(true);
     })
 
-    it('é retornado o metodo json contendo um array', async () => {
+    it('deve retornar o metodo "json" contendo um array', async () => {
       await ProductsController.getAll(request, response);
       expect(response.json.calledWith(sinon.match.array)).to.be.true;
     })
+  });
+
+  describe('addNewProduct', () => {
+    describe("quando é inserido com sucesso", async () => {
+      const response = {};
+      const request = {
+        body: { name: 'abacate albino' },
+      };
+
+      beforeEach(() => {
+        const result = {
+          id: 1,
+          name: 'abacate albino',
+        }
+
+        response.status = sinon.stub().returns(response);
+        response.json = sinon.stub().returns();
+
+        sinon.stub(ProductsService, 'addNewProduct').resolves(result);
+      })
+      afterEach(() => {
+        ProductsService.addNewProduct.restore();
+      })
+
+      it('deve retornar o método "status" com o código 201', async () => {
+        await ProductsController.addNewProduct(request, response);
+        expect(response.status.calledWith(201)).to.be.true;
+      })
+
+      it('deve retornar o método "json¨ passando um objeto', async () => {
+        await ProductsController.addNewProduct(request, response);
+        expect(response.json.calledWith(sinon.match.object)).to.be.true;
+      })
+    });
   });
 });
