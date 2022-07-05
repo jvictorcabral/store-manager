e35e9227468b88011bef89d4a8bee6d1983e6c49
Joao <joaovictorcgjvcg@gmail.com>
2022-07-05 16:31:03 -0400

    entregando o teste da funcao getAll da model

---
+++ tests/unit/models/ProductsModel.test.js
@@ -0,0 +1,72 @@
+const ProductsModel = require('../../../models/ProductsModel');
+const connection = require('../../../models/connection');
+const sinon = require('sinon');
+const { expect } = require('chai');
+
+describe('ProductsModel', () => {
+  describe('getAll', () => {
+    describe('quando não possui nenhum produto no banco de dados', () => {
+      const resultQuery = [[]];
+
+      before(() => {
+        sinon.stub(connection, 'query')
+          .resolves(resultQuery)
+      })
+
+      after(() => {
+        connection.query.restore();
+      })
+
+      it('deve retornar um array', async () => {
+        const result = await ProductsModel.getAll();
+        expect(result).to.be.an('array');
+      });
+
+      it('o array deve estar vazio', async () => {
+        const result = await ProductsModel.getAll();
+        expect(result).to.be.empty;
+      });
+    });
+    describe('quando possuem produtos no banco de dados', () => {
+
+      const resultQuery = [
+        {
+          id: 1,
+          name: 'Martelo de Thor',
+        }
+      ]
+
+      before(() => {
+        sinon.stub(connection, 'query')
+          .resolves([resultQuery])
+      })
+
+      after(() => {
+        connection.query.restore();
+      })
+
+      it('deve retornar um array', async () => {
+        const result = await ProductsModel.getAll();
+        expect(result).to.be.an('array');
+      });
+      
+      it('o array não deve estar vazio', async () => {
+        const result = await ProductsModel.getAll();
+        expect(result).to.be.not.empty;
+      });
+
+      it('o array possui objetos', async () => {
+        const [result] = await ProductsModel.getAll();
+        expect(result).to.be.an('object');
+      });
+
+      it('o objeto que esta no array contem os atributos id e name', async () => {
+        const [result] = await ProductsModel.getAll();
+        expect(result).to.be.includes.all.keys(
+          'id',
+          'name'
+        )
+      });
+    });
+  });
+});
